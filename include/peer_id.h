6ba6ea535d6c44544a5e3b084eafc0a29db82698
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2024-11-11 19:37:57 +0000

    refactor: peer id generation and retrival

--- include/peer_id.h
+++ include/peer_id.h
@@ -1,11 +1,23 @@
 #ifndef PEER_ID_H
 #define PEER_ID_H
 
+#define PEER_ID_SIZE 20
+
 /**
  * @brief Generate a peer ID
  *
- * @return char* The peer ID
+ * The peer ID is a 20-byte string that is used to identify the client
+ * to the tracker and other peers.
+ *
+ * The generated peer ID is stored in the GLOBAL_PEER_ID variable.
  */
-char *generate_peer_id(void);
+void generate_peer_id(void);
+
+/**
+ * @brief Get the peer ID
+ *
+ * @return const char* The peer ID
+ */
+const char *get_peer_id(void);
 
 #endif // !PEER_ID_H
--- src/peer_id.c
+++ src/peer_id.c
@@ -9,25 +9,21 @@
 static const char *client_id = "TC";
 static const char *version = "0001";
 
-char *generate_peer_id(void) {
-    char *peer_id = malloc(21);
-    if (peer_id == NULL) {
-        return NULL;
-    }
+static char peer_id[PEER_ID_SIZE];
 
-    int offset = snprintf(peer_id, 21, "-%s%s-", client_id, version);
+void generate_peer_id(void) {
+    int offset = snprintf(peer_id, PEER_ID_SIZE, "-%s%s-", client_id, version);
 
     // To get some thread safety (not secure but good enough for this)
     int pid = getpid();
     time_t t = time(NULL);
-    for (int i = offset; i < 20; ++i) {
+    for (int i = offset; i < PEER_ID_SIZE; ++i) {
         peer_id[i] = (char)((pid + (t >> i)) & 0xFF);
     }
 
-    // Null terminate the string
-    peer_id[20] = '\0';
-
     LOG_INFO("Generated peer id: %s", peer_id);
+}
 
+const char *get_peer_id(void) {
     return peer_id;
 }
